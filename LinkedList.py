6688eee8f4c38bbfcee50ce3de5a07ed068f4b52
Lars Thise Pedersen <ltpe@techcollege.dk>
2018-10-13 21:52:46 +0200

    Sort rutinen er nu implementeret og ser ud til at virke.

--- LinkedList.py
+++ LinkedList.py
@@ -7,12 +7,20 @@ class linkedList_Class():
     def addFirst(self, element):
         element.next = self.first
         self.first = element
+        test = 120
+
 
     def removeFirst(self):
-        test = 2
+        self.first = self.first.next
 
     def count(self):
-        test = 3
+        count = 0
+        temp = self.first
+        while (temp):
+            count += 1
+            temp = temp.next
+
+        return (count)
 
     def print(self):
         print("Elementer i den enkeltkædede Liste !!!")
@@ -22,4 +30,78 @@ class linkedList_Class():
             temp = temp.next
 
     def sort(self):
-        test = 5
+        dataToTestAgainst = self.first
+        elementPositionFromTop = 0
+
+        while dataToTestAgainst:
+            if dataToTestAgainst == self.first:
+                nodeBefore = None
+
+            minValue = dataToTestAgainst.data
+            minValueOriginal = minValue
+            dataToTest = dataToTestAgainst
+
+            while dataToTest.next != None:
+                if dataToTest.next.data < minValue:
+                    minValue = dataToTest.next.data
+                    placeholderBeforeMin = dataToTest
+                    minData = dataToTest.next
+                    placeholderAfterMin = dataToTest.next.next
+
+                dataToTest = dataToTest.next
+
+            if minValue != minValueOriginal:
+                if nodeBefore == None:
+                    # Vi tester på første element i den kædede liste
+                    placeholderTop = self.first
+                    self.first = minData
+                    minData.next = placeholderTop
+                    placeholderBeforeMin.next = placeholderAfterMin
+
+                else:
+                    placeholderTop = nodeBefore
+                    minData.next = placeholderTop.next
+                    placeholderTop.next = minData
+                    placeholderBeforeMin.next = placeholderAfterMin
+
+                nodeBefore = minData
+                dataToTestAgainst = minData.next
+            else:
+                nodeBefore = dataToTestAgainst
+                dataToTestAgainst = dataToTestAgainst.next
+
+                """
+                placeholderAfter = minData.next
+                placeholderMin = minData
+                placeholderDataAfterElementToBeRemovedDownInList = placeholderBefore.next
+
+                placeholderBefore.next = minData
+                minData.next = placeholderDataAfterElementToBeRemovedDownInList
+                placeholderBeforeMin = placeholderBefore
+                placeholderBeforeMin.next.next = placeholderAfter
+                """
+
+            """
+            nodeBefore = dataToTestAgainst
+            dataToTestAgainst = dataToTestAgainst.next
+            """
+            """
+            tempWork = temp
+            tempWorkCopy = None
+            while (tempWork):
+                tempWork = tempWork.next
+                if None == tempWork:
+                    break
+                if tempWork.data < minValue:
+                    minValue = tempWork.data
+                    tempWorkCopy = tempWork
+
+            if None != tempWorkCopy:
+                tempWorkCopyBackup = tempWorkCopy.next
+                tempWorkCopy.next = temp.next
+                temp.next = tempWorkCopyBackup.next
+                if temp == self.first:
+                    self.first = tempWorkCopy
+
+            temp = temp.next
+            """
--- Main.py
+++ Main.py
@@ -5,6 +5,10 @@ if __name__ == '__main__':
     # Nu oprettes en tom Liste.
     myLinkedList = LinkedList.linkedList_Class()
 
+    node0 = Element.element_Class(8)
+    myLinkedList.addFirst(node0)
+    myLinkedList.print()
+
     node1 = Element.element_Class(1)
     myLinkedList.addFirst(node1)
     myLinkedList.print()
@@ -13,6 +17,29 @@ if __name__ == '__main__':
     myLinkedList.addFirst(node2)
     myLinkedList.print()
 
+    node4 = Element.element_Class(4)
+    myLinkedList.addFirst(node4)
+    myLinkedList.print()
+
     node3 = Element.element_Class(3)
     myLinkedList.addFirst(node3)
     myLinkedList.print()
+
+    node5 = Element.element_Class(23)
+    myLinkedList.addFirst(node5)
+    myLinkedList.print()
+
+    print("Antal elementer i listen er : " + str(myLinkedList.count()))
+
+    #myLinkedList.removeFirst()
+    #myLinkedList.print()
+    #print("Antal elementer i listen er : " + str(myLinkedList.count()))
+    print("")
+    print("Før Sortering :")
+    myLinkedList.print()
+
+    myLinkedList.sort()
+    print("")
+    print("Efter Sortering :")
+    myLinkedList.print()
+
